f870c92e67faeeb4408f76e3d04909c2cf792b36
chino_desu <33673786+kafuuchino-desu@users.noreply.github.com>
2018-12-22 20:24:39 +0800

    update

--- daycount.py
+++ daycount.py
@@ -6,6 +6,9 @@ startday = datetime.datetime.strptime('2018-11-09', '%Y-%m-%d')
 def onServerInfo(server, info):
   if info.isPlayer == 1:
     if info.content.startswith('!!day'):
-      now = datetime.datetime.now()
-      output = now - startday
-      server.say('今天是这个服务器开服的第' + str(output.days) + '天')
+      server.say('今天是这个服务器开服的第' + getday() + '天')
+
+def getday():
+  now = datetime.datetime.now()
+  output = now - startday
+  return str(output.days)
