67e757fc4cbdf6984fb5258691b480107549f79e
Fallen_Breath <yqx791125833@gmail.com>
2020-04-07 12:27:08 +0800

    Update daycount.py

--- daycount.py
+++ daycount.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import copy
 import datetime
 
 startday = datetime.datetime.strptime('2018-11-09', '%Y-%m-%d')
@@ -7,6 +8,11 @@ def onServerInfo(server, info):
   if info.isPlayer == 1:
     if info.content.startswith('!!day'):
       server.say('今天是这个服务器开服的第' + getday() + '天')
+
+def on_info(server, info):
+  info2 = copy.deepcopy(info)
+  info2.isPlayer = info2.is_player
+  onServerInfo(server, info2)
 
 def getday():
   now = datetime.datetime.now()
